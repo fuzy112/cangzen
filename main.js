c918f5378dffbdd4477223a5ee532f169160f973
Zhengyi Fu <i@fuzy.me>
2024-11-05 13:03:39 +0800

    cangjie6

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ var currentMode;
 var decompositionCursor = document.getElementsByClassName('decomposition-cursor')[0];
 var questFrameCharactor = document.getElementsByClassName('quest-frame__charactor')[0];
 
-var key2RadicalTable = {"a":"日","b":"月","c":"金","d":"木","e":"水","f":"火","g":"土","h":"竹","i":"戈","j":"十","k":"大","l":"中","m":"一","n":"弓","o":"人","p":"心","q":"手","r":"口","s":"尸","t":"廿","u":"山","v":"女","w":"田","x":"難","y":"卜"};
+var key2RadicalTable = {"a":"日","b":"月","c":"金","d":"木","e":"水","f":"火","g":"土","h":"竹","i":"戈","j":"十","k":"大","l":"中","m":"一","n":"弓","o":"人","p":"心","q":"手","r":"口","s":"尸","t":"廿","u":"山","v":"女","w":"田","x":"止","y":"卜","z":"片"};
 
 window.addEventListener('load', function(){
 	var request = new XMLHttpRequest();
